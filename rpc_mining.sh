1733aeaf7c20eff8b99166cdb482a5f1f2e4834d
Nguyentraipc <127364998+Nguyentraipc@users.noreply.github.com>
2023-03-11 08:33:15 +0700

    Update rpc_mining.sh

--- rpc_mining.sh
+++ rpc_mining.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Replace YOUR_ADDRESS, YOUR_NODE:YOUR_PORT to run the miner"
 while :; do
-    ./astrominer -w dero1qyjmhd4yyk7rmkqnxwetkpw5fgk50w82zx9c74upqmluwcr2edknjqgevncls -r community-pools.mysrv.cloud:10300 -r1 dero.friendspool.club:10300 m 7 -p rpc;
+    ./astrominer -w dero1qyjpdut0dd7a380lz72pzud7ln4fc7qr5m7shq6sd75exgdr6jttkqggeac60 -r community-pools.mysrv.cloud:10300 -r1 dero.friendspool.club:10300 m 7 -p rpc;
     sleep 5;
 done
